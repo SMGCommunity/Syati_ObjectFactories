9a55f6bb8307d4c4f1bc977246bc0952ff55c617
VTXG <VTXG@users.noreply.github.com>
2026-07-22 17:16:21 +0100

    revert since I cba to fix this right now

--- codegen/GEN_SceneObjHolder.h
+++ codegen/GEN_SceneObjHolder.h
@@ -2,23 +2,13 @@
 
 #include "ActorCreation.h"
 
-#ifdef INCLUDE_PRIVATE
 {{IncludeList}}
-#endif
 
-enum ExtSceneObj {
-    ExtSceneObj_START = SCENE_OBJ_WII_MESSAGE_BOARD_MAIL_SENDER,
-{{SceneObjIdNameList}}
-    ExtSceneObj_END,
-    ExtSceneObj_COUNT = ExtSceneObj_END - ExtSceneObj_START - 1,
-};
 
-#ifdef INCLUDE_PRIVATE
 const CreateSceneObjEntry cNewCreateSceneObjTable[] = {
 	//DUMMY - This is never read
 	{ 1234567890, NULL },
 {{SceneObjList}}
 };
 
-#define cNewCreateSceneObjTableCount_END (&cNewCreateSceneObjTable[sizeof(cNewCreateSceneObjTable) / sizeof(*cNewCreateSceneObjTable)])
-#endif
+const s32 cNewCreateSceneObjTableCount = sizeof(cNewCreateSceneObjTable) / sizeof(CreateSceneObjEntry);
